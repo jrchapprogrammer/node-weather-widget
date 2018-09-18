e22be0e46b6235a512bdf5665a1ff71786d28ff8
John Chappelle III <jchappelleiii@gmail.com>
2018-09-18 14:21:03 -0400

    changed innerHTML

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const yargs = require('yargs');
 
 const argv = yargs;
 
-
 let apiKey = process.env.weather_api;
 let city = argv.c || 'philadelphia';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-
-request(url, function (err, response, body) {
-  if(err){
+request(url, function(err, response, body) {
+  if (err) {
     console.log('error:', error);
   } else {
     let weather = JSON.parse(body);
     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
     console.log(message);
-    // const weatherDiv = document.getElementsByClassName("weather-container");
-    // weatherDiv.innerHtml(message);
+    const weatherDiv = document.getElementsByClassName('weather-container');
+    weatherDiv.innerHTML = message;
   }
 });
